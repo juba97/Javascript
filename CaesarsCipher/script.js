b58c65dffa06c136961c46d6ee5d4961a7c0d483
juba97 <64692459+juba97@users.noreply.github.com>
2020-10-22 03:24:32 +0400

    Add files via upload

---
+++ CaesarsCipher/script.js
@@ -0,0 +1,33 @@
+function caesarCipher(str, kMove) {
+	let move = parseInt(kMove);
+	let string = str.toLowerCase();
+	let encrypted = "";
+	let newIndex;
+	let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+	//alphabet = ["a", "b", "c"]
+	//alphabet[0]
+	//alphabet[1]
+	for (var i = 0; i < alphabet.length; i++) {
+		if (string[i] == " ") {
+			encrypted += string[i];
+		}
+		else {
+			if (alphabet.indexOf(string[i]) > -1) {
+				newIndex = alphabet.indexOf(string[i]) + move;
+				if (newIndex > alphabet.length) {
+					newIndex = newIndex - alphabet.length;
+				}
+				else if (newIndex < 0) {
+					newIndex = alphabet.length + move;
+				}
+				encrypted += alphabet[newIndex];
+			}
+		}
+
+	}
+	alert(encrypted);
+}
+
+let str = prompt("Enter string to encrypt");
+let k = prompt("Enter k");
+caesarCipher(str, k);
