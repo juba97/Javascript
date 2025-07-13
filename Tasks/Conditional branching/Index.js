a2aaf8ead8ee7bf2c6bff63de89cd7a60c95225c
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-07-13 14:22:13 +0400

    Add files via upload

---
+++ Tasks/Conditional branching/Index.js
@@ -0,0 +1,28 @@
+// Discount calculation based on user role
+const userIsAdmin = true;
+const discount = userIsAdmin ? 25 : 0;
+console.log(`The discount is ${discount}%`);
+
+// calculate shipping cost based on distance
+const userDistance = 45;
+const shippingCost = userDistance <= 50 ? 10 : 15;
+console.log(`The shipping cost is $${shippingCost}`);
+
+// Conditional logic to determine discount based on user role
+const userIsAdmin = false;
+let userStudents = true;
+let discount;
+
+if (userIsAdmin) {
+    console.log("Welcome, Admin!");
+    discount = 25;
+}
+else if (userStudents) {
+    console.log("Welcome, Student!");
+    discount = 15;
+}
+else {
+    console.log("Welcome, User!");
+    discount = 0;
+}
+console.log(`The discount is ${discount}%`);
