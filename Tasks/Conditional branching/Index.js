4404a6cb7c01c584ec1d1692b5fa455ffeed4003
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-07-13 14:44:56 +0400

    Ternary, if/else

--- Tasks/Conditional branching/Index.js
+++ Tasks/Conditional branching/Index.js
@@ -26,3 +26,13 @@ else {
     discount = 0;
 }
 console.log(`The discount is ${discount}%`);
+
+// 4) Will alert be shown?
+
+if ("0") {
+  console.log( 'Hello' );
+}
+
+/*Yes, it will.
+Any string except an empty one (and "0" is not empty) becomes true in the logical context.
+*/
