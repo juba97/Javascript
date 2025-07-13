272e8f5466a584d1539680d8a90136e9248da0b2
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-07-13 14:53:53 +0400

    Ternary, if/else

--- Tasks/Conditional branching/Index.js
+++ Tasks/Conditional branching/Index.js
@@ -49,3 +49,20 @@ if (name === "ECMAScript") {
 }else {
   alert("You don’t know? ECMAScript!");
 }
+
+/*
+    6) Using if..else, write the code which gets a number via prompt and then shows in alert:
+
+    • 1, if the value is greater than zero,
+    • -1, if less than zero,
+    • 0, if equals zero.
+*/ 
+
+let number = prompt("Type a number", 0);
+if (number > 0) {
+  alert(1);
+}else if (number < 0) {
+    alert(-1);
+}else {
+    alert(0);
+}
