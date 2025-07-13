c8e0819726b0a7bf24a993d1f551558d2b9bcce6
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-07-13 14:45:24 +0400

    Ternary, if/else

--- Tasks/Conditional branching/Index.js
+++ Tasks/Conditional branching/Index.js
@@ -36,3 +36,16 @@ if ("0") {
 /*Yes, it will.
 Any string except an empty one (and "0" is not empty) becomes true in the logical context.
 */
+
+/*
+   5 ) Using the if..else construct, write the code which asks: ‘What is the “official” name of JavaScript?’
+
+       If the visitor enters “ECMAScript”, then output “Right!”, otherwise – output: “You don’t know? ECMAScript!”
+*/ 
+
+let name = prompt("What is the 'official' name of JavaScript?");
+if (name === "ECMAScript") {
+  alert("Right!");
+}else {
+  alert("You don’t know? ECMAScript!");
+}
