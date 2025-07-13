668c33e2c1920b6ebc1703fde2d7f801dea859bf
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-07-13 14:14:19 +0400

    Ternary, if/else

--- Tasks/Conditional Statements/Index.js
+++ Tasks/Conditional Statements/Index.js
@@ -3,6 +3,7 @@ const userIsAdmin = true;
 const discount = userIsAdmin ? 25 : 0;
 console.log(`The discount is ${discount}%`);
 
+
 // calculate shipping cost based on distance
 const userDistance = 45;
 const shippingCost = userDistance <= 50 ? 10 : 15;
