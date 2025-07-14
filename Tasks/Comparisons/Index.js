482a6a54ce2b8c8aeaefaea7b6edac8099069a3c
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-07-14 20:34:03 +0400

    Update Index.js

--- Tasks/Comparisons/Index.js
+++ Tasks/Comparisons/Index.js
@@ -20,7 +20,6 @@ null === +"\n0\n" → false
 /*
     Some of the reasons:
 
-
         Obviously, true.
         Dictionary comparison, hence false. "a" is smaller than "p".
         Again, dictionary comparison, first char "2" is greater than the first char "1".
