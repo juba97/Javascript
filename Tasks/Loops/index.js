cb0d65732866418120c1597d246026497076c8e0
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-07-28 14:22:30 +0400

    Update index.js

--- Tasks/Loops/index.js
+++ Tasks/Loops/index.js
@@ -5,6 +5,7 @@ while (i) {
   console.log(i--);
 }
 
+
 /*  answer:
   Every loop iteration decreases i by 1. The check while(i) stops the loop when i = 0.
   Hence, the steps of the loop form the following sequence (“loop unrolled”):
