037a5fe1837f7677bc3419ed8ebe929fa5081f49
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-07-30 23:44:43 +0400

    Update index.js

--- Tasks/Array/index.js
+++ Tasks/Array/index.js
@@ -1,5 +1,4 @@
 // 1) Find the JavaScript in an array
-
 let str = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
 let search = 'JavaScript';
 for (let i = 0; i < str.length; i++) {
@@ -11,7 +10,6 @@ for (let i = 0; i < str.length; i++) {
 
 // 2) Find duplicate elements in an array
 const arr = [20, 57, 44, 10, 100, 57, 44, 54];
-
 for(let i = 0; i < arr.length; i++) {
     for(let j = i + 1; j < arr.length; j++) {
         if(arr[i] == arr[j]) {
