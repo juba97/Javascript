ea93f02f9123405c8a75de8948012df6236fe16e
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-08-01 23:42:38 +0400

    Update index.js

--- Tasks/Array/index.js
+++ Tasks/Array/index.js
@@ -1,4 +1,5 @@
 // 1) Find the "JavaScript" in an array
+
 let str = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
 let search = 'JavaScript';
 for (let i = 0; i < str.length; i++) {
@@ -17,7 +18,3 @@ for(let i = 0; i < arr.length; i++) {
         }
     }
 }
-
-
-
-
