fa1cba34d3706c87b8f2b4750e35cfb636dcf81c
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-08-02 16:14:12 +0400

    arrays

--- Tasks/Array/index.js
+++ Tasks/Array/index.js
@@ -65,3 +65,31 @@ arr[2](); // ?
     So we have a call of the function arr[2] as an object method. Naturally, it receives this referencing the object arr and outputs the array
     The array has 3 values: initially it had two, plus the function.
 */
+
+/*
+    6) Write the function sumInput() that:
+
+        • Asks the user for values using prompt and stores the values in the array.
+        • Finishes asking when the user enters a non-numeric value, an empty string, or presses “Cancel”.
+        • Calculates and returns the sum of array items.
+       P.S. A zero 0 is a valid number, please don’t stop the input on zero.
+*/
+
+function sumInput() {
+  let numbers = [];
+
+  while (true) {
+
+    let value = prompt("A number please?", 0);
+    if (value === "" || value === null || !isFinite(value)) break;
+    numbers.push(+value);
+  }
+
+  let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
+}
+
+alert( sumInput() );
