435821afea9e60fee50a66aa931533c7ffde8588
Juba Koguashvili <jubikokoguashvili97@gmail.com>
2025-08-02 16:02:18 +0400

    array

--- Tasks/Array/index.js
+++ Tasks/Array/index.js
@@ -18,3 +18,15 @@ for(let i = 0; i < arr.length; i++) {
     }
 }
 
+// 1)What is this code going to show?
+
+let fruits = ["Apples", "Pear", "Orange"];
+
+// push a new value into the "copy"
+let shoppingCart = fruits;
+shoppingCart.push("Banana");
+
+// what's in fruits?
+alert( fruits.length ); // ?,  4
+
+// That’s because arrays are objects. So both shoppingCart and fruits are the references to the same array.
